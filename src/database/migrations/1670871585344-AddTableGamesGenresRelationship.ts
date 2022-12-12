bc6e48dbc13355b1a1aa4b5dc24bedd051e507b2
GleidsonLM <gleidsonlm@gmail.com>
2022-12-12 16:23:01 -0300

    add: games_genres table contains relationship many to many

---
+++ src/database/migrations/1670871585344-AddTableGamesGenresRelationship.ts
@@ -0,0 +1,43 @@
+import {MigrationInterface, QueryRunner, Table, TableForeignKey} from "typeorm";
+
+export class AddTableGamesGenresRelationship1670871585344 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: 'games_genres',
+                columns: [
+                    {
+                        name: 'gamesId',
+                        type: 'uuid',
+                    },
+                    {
+                        name: 'genresId',
+                        type: 'uuid',
+                    }
+                ]
+            })
+        )
+        await queryRunner.createForeignKey(
+            'games_genres',
+            new TableForeignKey({
+                columnNames: ['gamesId'],
+                referencedTableName: 'games',
+                referencedColumnNames: ['id'],
+            })
+        )
+        await queryRunner.createForeignKey(
+            'games_genres',
+            new TableForeignKey({
+                columnNames: ['genresId'],
+                referencedTableName: 'genres',
+                referencedColumnNames: ['id'],
+            })
+        )        
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('games_genres')
+    }
+
+}
